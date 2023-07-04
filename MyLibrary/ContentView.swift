bd5c0282b0a91935c652b80bde8ee2cdc7f2f794
Nicoly-Almeida <nicoly.almeida@picpay.com>
2023-07-03 21:35:43 -0300

    version latest

--- MyLibrary/ContentView.swift
+++ MyLibrary/ContentView.swift
@@ -10,79 +10,81 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+        @Environment(\.presentationMode) var presentationMode
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        @FetchRequest(
+            sortDescriptors: [NSSortDescriptor(keyPath: \Book.name, ascending: true)],
+            animation: .default)
+        private var books: FetchedResults<Book>
 
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        var body: some View {
+            NavigationView {
+                ZStack {
+                    top
+                    listagem
+                }
+            }
+        }
+    }
+
+    extension ContentView {
+        var top: some View {
+            ZStack {
+                Color.green
+                    .opacity(0.4)
+                    .edgesIgnoringSafeArea(.top)
+                Color.white
+            }
+        }
+    }
+
+    extension ContentView {
+        var listagem: some View {
+            VStack {
+                List {
+                    ForEach(self.books) { book in
+                        ListView(book: book)
+                    }
+                    .onDelete(perform: self.deleteItems)
+                }
+                .padding(.top)
+                .navigationTitle("Minha biblioteca")
+                .toolbar {
+                    ToolbarItem() {
+                        NavigationLink(destination: FormView()) {
+                            Image(systemName: "plus")
+                        }
+                    }
+                    ToolbarItem() {
+                        EditButton()
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+        }
+        
+        private func deleteItems(offsets: IndexSet) {
+            withAnimation {
+                offsets.map { books[$0] }.forEach(viewContext.delete)
+
+                do {
+                    try viewContext.save()
+                } catch {
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+    private let itemFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        return formatter
+    }()
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
 }
